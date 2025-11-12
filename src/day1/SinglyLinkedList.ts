8c02b1d28b18b721ed759dd68ff8e9ca05d2f0ca
Tony Baidoo <tony.baidoo@hubtel.com>
2025-11-12 14:04:13 +0000

    prepend append remove insertAt redo

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,43 +1,149 @@
+import { isReturnStatement } from "typescript";
+
+class Node<T> {
+    public data: T | null;
+    public next: Node<T> | null;
+
+    constructor(data: T) {
+        this.data = data;
+    }
+}
+
 export default class SinglyLinkedList<T> {
     public length: number = 0;
-    public data: T | null;
-    public next: SinglyLinkedList<T> | null;
+    public head: Node<T>;
 
     constructor() {}
 
     prepend(item: T): void {
-        const newNode = new SinglyLinkedList<T>()
-        // copy current head (data and next pointer)
-        newNode.data = this.data
-        newNode.next = this.next
-        // update existing head's date to new item
-        this.data = item
-        // update next to point to copied current head
-        this.next = newNode
-        this.length++
-    }
-    insertAt(item: T, idx: number): void {}
-    append(item: T): void {
-        if (this.length === 0) {
-            this.data = item;
+        const node = new Node(item);
+        if (!this.head) {
+            console.log("no head");
+            this.head = node;
             this.length++;
             return;
         }
-        let temp = this as SinglyLinkedList<T>;
+        let temp = this.head;
+        node.next = temp;
+        this.head = node;
+        this.length++;
+    }
+    append(item: T): void {
+        const node = new Node(item);
+        if (!this.head) {
+            this.head = node;
+            this.length++;
+            return;
+        }
+        let temp = this.head;
         while (temp.next) {
             temp = temp.next;
         }
-        const newNode = new SinglyLinkedList<T>();
-        newNode.data = item;
-        temp.next = newNode;
-        this.length++
+        temp.next = node;
+        this.length++;
     }
+    insertAt(item: T, idx: number): void {
+        const node = new Node(item);
+        let i = 0;
+        let temp = this.head;
+        if (idx < 0 || this.length < idx) {
+            return;
+        }
+        if (idx === 0) {
+            node.next = this.head;
+            this.head = node;
+            this.length++
+            return;
+        }
+        while (temp.next && i < idx - 1) {
+            console.log({
+                idx: i,
+                data: temp.data,
+            });
+            temp = temp.next;
+            i++;
+        }
+        console.log("curr node =>", temp);
+        node.next = temp?.next;
+        temp.next = node;
+        this.length++;
+    }
+    remove(item: T): T | undefined {
+        let temp = this.head;
+        if (temp.data === item) {
+            const nextNode = temp?.next;
+            const removed = temp.data!;
+            this.head = nextNode!;
+            this.length--;
+            return removed;
+        }
+
+        while (temp.next) {
+            const nextNode = temp.next;
+            if (nextNode.data === item) {
+                const removed = nextNode.data!;
+                temp.next = nextNode.next;
+                this.length--;
+                return removed;
+            }
+            temp = temp.next;
+        }
+
+        return undefined;
+    }
+    // get(idx: number): T | undefined {
+    //     let temp = this as SinglyLinkedList<T>;
+    //     let i = 0;
+    //     while (temp && i <= idx) {
+    //         console.log(temp.data);
+    //         if (i === idx) {
+    //             return temp.data!;
+    //         }
+    //         temp = temp.next!;
+    //         i++;
+    //     }
+
+    //     return undefined;
+    // }
+    // removeAt(idx: number): T | undefined {
+    //     let temp = this as SinglyLinkedList<T>;
+    //     let i = 0;
+    //     if (idx === 0) {
+    //         const removed = temp.data ?? undefined;
+    //         const nextNode = temp?.next;
+    //         temp = nextNode;
+    //         this.length--;
+    //         return removed;
+    //     }
+    //     while (temp.next && i < idx) {
+    //         const nextNode = temp.next;
+    //         const nextIdx = i + 1;
+    //         if (nextIdx === idx) {
+    //             temp.next = nextNode.next;
+    //             this.length--;
+    //             return nextNode.data ?? undefined;
+    //         }
+    //         temp = temp.next;
+    //         i++;
+    //     }
+
+    //     return undefined;
+    // }
 }
 const list = new SinglyLinkedList<number>();
-list.append(1);
-list.append(3);
+list.prepend(1);
+// list.prepend(3);
 list.append(9);
-// list.append(80);
-list.prepend(300)
+// list.remove(3);
+// list.remove(1);
+// list.remove(9);
+// list.insertAt(777, 0);
+// list.insertAt(777, 190);
+// list.insertAt(777, 0)
+// list.prepend(300);
+list.remove(9);
+// console.log(list.get(0))
 console.log(list);
-console.log("length", list.length);
+// console.log(list.removeAt(0));
+// console.log(list);
+// console.log("length", list.length);
